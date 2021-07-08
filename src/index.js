452be97dc5d0c818acf1a40a3d72df93e8661bf5
shinshin86 <beagles1986@gmail.com>
2021-07-08 14:04:57 +0900

    Add generateDeepElement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import { createElement, render } from "./Didact";
 
+const generateDeepElement = (depth) => {
+  depth = depth - 1;
+  
+  if(depth < 1) {
+    return createElement("p", null, 'HELLO!!!!!')
+  } else {
+    return createElement("div", null, generateDeepElement(depth))
+  }
+}
 
 const element = createElement(
   "div",
   { id: "foo" },
   createElement("h1", null, "hello"),
-  createElement("p", null, createElement("b", null, "world"))
+  createElement("p", null, createElement("b", null, "world")),
+  generateDeepElement(10)
 )
 
 const container = document.getElementById("root");
